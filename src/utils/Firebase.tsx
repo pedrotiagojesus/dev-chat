53e907b9c482c68c25218438fb691f4fb3425fa9
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-09-18 21:39:22 +0100

    Update favicon

--- src/utils/Firebase.tsx
+++ src/utils/Firebase.tsx
@@ -18,7 +18,7 @@ export async function signIn(email: string, password: string) {
     return await signInWithEmailAndPassword(auth, email, password);
 }
 
-export async function signOut() {
+export async function logOut() {
     // const auth = getAuth();
     return await signOut();
 }

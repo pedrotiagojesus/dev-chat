b79bd5aff6b60ea5b25b8e055cae172200825152
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-09-19 00:01:10 +0100

    Add header when login

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+// CSS
+import "./Header.css";
+
+// Hooks
+import { useAuthentication } from "../../hooks/firebase/useAuthentication";
+
+const Header = () => {
+    const { logout } = useAuthentication();
+
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <i className="fa-solid fa-code"></i>
+                        <span>Dev Chat</span>
+                    </a>
+                    <button className="btn btn-logout" onClick={logout}>
+                        Logout
+                    </button>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

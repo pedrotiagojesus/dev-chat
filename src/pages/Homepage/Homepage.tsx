40abb31844afb00beba1b07a1187fd2c6f632a72
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-09-20 23:06:01 +0100

    Update homepage text

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -16,12 +16,12 @@ const Homepage = () => {
                             </div>
                             <div className="card-body">
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Temporibus quidem expedita
-                                    mollitia veritatis unde recusandae, animi
-                                    voluptatibus eius odit iusto atque ipsum
-                                    deleniti velit obcaecati soluta harum rem
-                                    sed odio!
+                                    Hi! This is a real-time chat app that I
+                                    developed using React and Firebase. It
+                                    allows instant conversations with a modern
+                                    and responsive interface, leveraging the
+                                    power of React for a dynamic experience and
+                                    Firebase for real-time data management.
                                 </p>
                                 <div className="button-group">
                                     <button
